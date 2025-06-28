48ee95ede7232ab8dbcf15d367a9f7e6525547de
muthu-kumar-mk <muthu.kumar@highway905.com>
2025-06-28 21:49:48 +0200

    Updated task-status-chart.tsx

--- features/dashboard/components/task-status-chart.tsx
+++ features/dashboard/components/task-status-chart.tsx
@@ -192,7 +192,7 @@ export function TaskStatusChart({ className }: TaskStatusChartProps) {
               width={-10}
               axisLine={false}
               style={{paddingLeft: '15px'}}
-              dx={10}
+              dx={-90}
             />
             <Tooltip content={<CustomTooltip />} />
             
