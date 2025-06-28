210b023c2ff9b37b019a6df8c5776500e04d37b8
muthu-kumar-mk <muthu.kumar@highway905.com>
2025-06-28 21:48:59 +0200

    Updated task-status-chart.tsx

--- features/dashboard/components/task-status-chart.tsx
+++ features/dashboard/components/task-status-chart.tsx
@@ -189,7 +189,7 @@ export function TaskStatusChart({ className }: TaskStatusChartProps) {
               dataKey="name" 
               type="category" 
               tick={{ fontSize: 12 }}
-              width={120}
+              width={190}
               axisLine={false}
               style={{paddingLeft: '15px'}}
               dx={-90}
